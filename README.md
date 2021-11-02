044276639f77e8098ea9c913af1e614a8816b3b8
ahmedzidan <ahmedzidan_2015@hotmail.com>
2021-11-02 10:31:57 +0800

    mongodb port-forward

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# mongo-pf
+# mongodb k8s Port-forward
+
+Goal:
+- Sometimes you want to expose your mongodb using port-forward in k8s
+- Most of the time you will need to connect to the primary repica
+- But there is no way that tell you which pod is the primary
+- So you need to do port-forward first and see which one is primary
+- Then you do port forward on that one
+- The main goad of this script is to eliminate that and it does do port-forward directly to the primary repica
+## Prerquist
+- Python3 
+- Kubectl 
+- Mongo shell client
+
+## How to use it. 
+- Download the script
+- Then run the Following command. 
+```shell
+./mongo.py port-forward -po ${podname} -ns ${namespace} -u ${mongo-username} -p ${mongo-password} 
+``` 
+- Where ${podname} and pod from the mongo cluster
+- ${namespace} k8s namespace that has the mongo cluster
+- ${mongo-username} username to connect to mongodb default to ``admin``
+- ${mongo-password} password for mongodb
---
+++ mongo.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import argparse
+import subprocess
+import shlex
+
+def initPortForward(namespace, pod):
+  cmd= "kubectl -n "+namespace+" port-forward "+ pod + " 27017:27017"
+  return subprocess.Popen(shlex.split(cmd))
+
+def getPrimaryPod(username, password):
+  getcmd ="mongo 'mongodb://127.0.0.1:27017' --username "+username+ " --password "+password+" --eval 'rs.isMaster().primary'"
+  result = subprocess.run(shlex.split(getcmd), capture_output=True, universal_newlines=True)
+  result = result.stdout.encode('utf-8')
+  primaryPodUrl = subprocess.run(["grep", "svc.cluster.local"], capture_output=True, input=result).stdout
+  return primaryPodUrl.decode('utf-8').split(".")[0]
+
+def portForwardPrimary(namespace, primaryPod):
+  cmdPortForwardMaster= "kubectl -n "+ namespace +" port-forward "+ primaryPod + " 27017:27017"
+  subprocess.run(shlex.split(cmdPortForwardMaster))
+     
+def connect(pods, namespace, username, password):
+  proc = initPortForward(namespace=namespace, pod=pods)
+  subprocess.run(["sleep", "2"])
+  primaryPod = getPrimaryPod(username=username, password=password)
+  
+  subprocess.run(["echo", "primary is "+primaryPod])
+  # trinimante the temp process
+  proc.terminate()
+  # run the port forwaed on the master node
+  portForwardPrimary(namespace, primaryPod)
+  
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser(description='Echo your input')
+   
+   subparsers = parser.add_subparsers(dest='func')
+   connect_parser =  subparsers.add_parser('port-forward',  
+                                         help='this is to port-forward to mongodb primary pod')
+   connect_parser.add_argument('--pod', '-po',
+                               help="mongodb pod name"
+                              )
+   connect_parser.add_argument('--namespace', '-ns',
+                              help="namespace"
+                              )
+   connect_parser.add_argument('--username', '-u',
+                            help="mogodb username",
+                            default="admin"
+                            )
+   connect_parser.add_argument('--password', '-p',
+                            help="mogodb password"
+                            )
+
+   args = parser.parse_args()
+   if args.func == "port-forward":
+     connect(args.pod, args.namespace, args.username, args.password)   
